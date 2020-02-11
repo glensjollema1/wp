8884b838e1e248285e1f00bfe4cc4aa1024e0a05
glensjollema1 <s3353578@student.rmit.edu.au>
2020-02-11 20:14:07 +1100

    added boilerplate to tools.php, nothing implemented yet

--- a4/tools.php
+++ a4/tools.php
@@ -3,4 +3,37 @@
 
 // Put your PHP functions and modules here
 
+//return array as either the original data format or as a string
+  function preShow( $arr, $returnAsString=false ) {
+    $ret  = '<pre>' . print_r($arr, true) . '</pre>';
+    if ($returnAsString)
+      return $ret;
+    else 
+      echo $ret; 
+  }
+
+//echo out the code with line numbers
+  function printMyCode() {
+    $lines = file($_SERVER['SCRIPT_FILENAME']);
+    echo "<pre id='mycode'><ol>";
+    foreach ($lines as $line)
+      echo '<li>'.rtrim(htmlentities($line)).'</li>';
+    echo '</ol></pre>';
+  }
+
+  //convert php array to js object
+  function php2js( $arr, $arrName ) {
+    echo "<script>\n";
+    echo "/* Generated with A4's php2js() function */";
+    echo "  var $arrName = ".json_encode($arr, JSON_PRETTY_PRINT);
+    echo "</script>\n\n";
+  }
+
+  //reset the form / session
+  if (isset($_POST['session-reset'])) {
+    foreach($_SESSION as $something => &$whatever) {
+      unset($whatever);
+    }
+  }
+
 ?>
